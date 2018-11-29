29b8d0a39b8cac07a50d68cc6113e0ac103cf44c
Kenny <kennethichoi@gmail.com>
2018-11-28 22:38:09 -0800

    Main page styling completed

---
+++ scripts/app.js
@@ -0,0 +1,29 @@
+/**********************
+Pomodoro Timer
+Base Functionality
+It should be able to count down from 25 minutes to 0
+It should always stop counting when reaching 00:00
+It should display each second of counting down
+It should have ability to start, stop & reset 
+
+Extras
+Tier #1
+It should give an alarm sound when the timer counts down to 0
+It should give the option for pomodoro, short break & long break
+It should display the current time remaining in the page title
+
+Tier #2
+It should have a settings panel for the following:
+User preferences:
+Timer indication in title
+Browser notifications
+Auto start pomodoros and breaks
+Select Sound
+Select Volume
+Set Custom Times
+It should have options for save, reset & sound test
+
+Tier #3
+It should have keyboard shortcuts
+It should have a FAQ section
+**********************/
